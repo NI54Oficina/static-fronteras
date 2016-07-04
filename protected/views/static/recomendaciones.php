857f7513b99ca3755d5e6dd45dc48e6eeeb59445
Melania Miranda <melania.smc@hotmail.com>
2016-07-04 13:49:25 -0300

    13:50 4-7-2016

--- protected/views/static/recomendaciones.php
+++ protected/views/static/recomendaciones.php
@@ -7,54 +7,134 @@
 
 	<h1 class="hidden-xs">Recomendaciones técnicas</h1>
 
-	<!-- Cajas de la sección -->
-	<!-- <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12"> -->
 
-		<!-- Caja  principal nombre -->
-		<!-- <div class="col-lg-2 col-sm-3 col-md-3 col-xs-6 col-lan-xs-3 checkbox-container">
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 general-container-producir container-recomendaciones">
+ 	<!-- Barra de color -->
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 barra-recomendaciones">     </div>
 
-		<div id="nombre-checkbox" class="square" >
-			<h2 class="col-lg-12 col-md-12 col-sm-12 col-xs-12 center-to-parent">Nombre <br /> de producción
-			</h2>
+	<!-- Primer Caja -->
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 box-recomendaciones">
+
+		<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 img-recomendaciones">
+			<img  class="img-r-1 col-xs-6" src="<?php echo Yii::app()->request->baseUrl; ?>/img/vaca-r.png" alt="" />
+			<img  class="img-r-1 col-xs-6 hidden-lg hidden-md hidden-sm" src="<?php echo Yii::app()->request->baseUrl; ?>/img/vaca2-r.png" alt="" />
 		</div>
 
+		<div class="col-lg-9 col-md-9 col-sm-9 col-xs-12 parrafo-recomendaciones parrafo-recomendaciones-1">
+			<p class="texto-blue-r">
+				Llega el otoño y nos encontramos, por un lado, con las vacas preñadas atravesando su último
+				tercio de la gestación y por el otro, con los terneros recién destetados que entran en la recría,
+				etapa de transición hacia la invernada o hacia el entore en el caso de los  animales de reposición.
+				Generalmente esta categoría no es considerada prioritaria a nivel sanitario ni nutricional; sin embargo,
+				 es la etapa donde el animal en crecimiento es más eﬁciente para convertir alimento en músculo y hueso.
+			</p>
+			<p class="texto-blue-r">
+				Si queremos faenar animales de mayor peso en menor tiempo, llegar al entore a edad más temprana o
+				 que las vacas paran un ternero sano y fuerte, tenemos que implementar planes sanitarios preventivos
+				  y estratégicos acordes a la época, a la oferta forrajera y al destino del animal, para poder seguir
+					 en el camino a la Frontera Productiva del establecimiento.
+			</p>
 		</div>
 
-		<div class="col-lg-2 col-sm-3 col-md-3 col-xs-6 col-lan-xs-3 checkbox-container">
-			<div class="checkbox-box square">
+	</div>
 
-			</div>
+	<!-- Segunda Caja -->
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 box-recomendaciones">
+		<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 img-recomendaciones">
+			<p class="title-side-r">
+				La recría
+			</p>
+			<img class="img-r-2" src="<?php echo Yii::app()->request->baseUrl; ?>/img/productos-r.png" alt="" />
+		</div>
+		<div class="col-lg-9 col-md-9 col-sm-9 col-xs-12 parrafo-recomendaciones">
+			<p>
+				EL CONTROL PARASITOLÓGICO es muy importante en esta época (otoño-invierno), dado que el destete y la recría/invernada
+				 son las categorías más sensibles a la acción de los parásitos.En Argentina, las pérdidas estimadas por ciclo productivo
+				  en una invernada con infestación parasitaria baja (subclínicas) son de 20 a 30 kg de peso vivo y pueden ascender
+					entre 40 y 60 kg ante infestaciones moderadas o altas (clínicas).El tratamiento con endectocidas de larga acción
+					(BOVIFORT®, BAGOMECTINA® FORTEAD3E, Bagomectina 3,15 LA AD3E y FLOK®) permite controlar por más tiempo las parasitosis
+					 internas y externas (piojos, sarna, ura, garrapatas).Se recomienda la rotación de principios activos para evitar la
+					  generación de resistencia. Esta rotación puede hacerse con productos como PARAXANE® 10% (bencimidazol) o PARAXANE®
+						INYECTABLE (Ricobendazol) o FOSFAMISOL® (levamisol).
+			</p>
+
+			<p>
+				LA CARENCIA DE MINERALES Y/O VITAMINAS genera mermas en la ganancia diaria de peso que oscilan entre 3 y 17 kg
+				por animal.Las carencias pueden deberse a un bajo aporte diario en la dieta o a la interferencia de otros minerales
+				en la absorción (molibdeno, azufre, etc.).Hay regiones ganaderas muy importantes de nuestro país donde es bien conocida
+				 la carencia de cobre y zinc, como por ejemplo Cuenca del Salado. Además, hay que tener en cuenta que en situación de
+				  lluvias intensas y encharcamiento de pasturas las carencias minerales aumentan.Por lo expuesto, la estrategia de
+					 suplementación con cobre y zinc (SUPLENUT®) está indicada en situaciones de carencia de estos minerales.
+
+			</p>
+
+			<p>
+				INMUNIDAD CONTRA ENFERMEDADES INFECCIOSAS:  </br>
+    		<li>Síndrome respiratorio -> BIOPOLIGEN® HS / BIOPOLIGEN® AIR  </li>
+    		<li>Síndrome clostridial -> POLICLOSTRIGEN® / BIOCLOSTRIGEN® J5 / TRIPLEpoligen®   </li>
+    		<li>Síndrome de queratoconjuntivitis -> BIOQUERATOGEN® OLEO MAX   </li>
+    		<li>Es preciso tener en cuenta que deben aplicarse dos dosis para cada síndrome, con un intervalo de 15 a 30 días,
+					cuando no se haya realizado vacunación al destete (primo vacunación).</li>
+			</p>
+
 
 		</div>
 
-		<div class="col-lg-2 col-sm-3 col-md-3 col-xs-6 col-lan-xs-3 checkbox-container">
-			<div class="checkbox-box square">
+	</div>
 
-			</div>
+	<!-- Tercera Caja -->
 
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 box-recomendaciones">
+		<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 img-recomendaciones">
+			<p class="title-side-r">
+					Vacas </br>al preparto
+			</p>
+				<img class="img-r-3"  src="<?php echo Yii::app()->request->baseUrl; ?>/img/rotatec-r.png" alt="" />
+		</div>
+		<div class="col-lg-9 col-md-9 col-sm-9 col-xs-12 parrafo-recomendaciones">
+			<p>
+				Para prevenir enfermedades en terneros recién nacidos se debe inmunizar (vacunar) a las madres en el último tercio de
+				 gestación. Esto genera defensas en las vacas que son transmitidas al ternero a través del calostro.
+			</p>
+
+			<p>
+				Se recomienda la aplicación preparto (30 días antes del parto) de vacunas que brinden inmunidad pasiva vía calostro contra
+				 síndromes especíﬁcos como diarrea neonatal y síndrome respiratorio, característicos de los primeros días de vida.
+			</p>
+
+			<p>
+				En el caso de las vaquillonas o vacas que no han recibido vacunaciones previas para prevenir estos síndromes, se recomienda
+				 aplicar dos dosis. La primera dosis 60 días antes de la fecha probable de parto y la segunda 30 días antes.
+			</p>
+			<p>
+				Las vacunas a aplicar según el establecimiento, casuística y tipo de explotación son:  </br>
+    		<li>Síndrome de diarrea neonatal -> ROTATEC J5®   </li>
+    		<li>Síndrome respiratorio -> BIOPOLIGEN® HS / BIOPOLIGEN® AIR</li>
+			</p>
+
+			<p>
+				En las zonas con carencias minerales de cobre y zinc, es vital la suplementación inyectable de SUPLENUT® aplicándolo a las
+				vacas en el período de preparto para generar reserva hepática fetal y evitar la carencia en los primeros dos meses de vida de
+				 los terneros en los cuales la leche materna no cubre sus requerimientos.
+			</p>
+
+			<p>
+				Por último, en las vaquillonas en período de preparto, debe realizarse un estricto control parasitológico de manera que lleguen
+				 al parto con peso adecuado y buen estado corporal.
+			</p>
+
+			<p>
+				Como conclusión, el buen asesoramiento profesional de su médico veterinario junto a un Plan Sanitario Preventivo que contemple
+				estos aspectos, nos acercarán a la Frontera Productiva.
+			</p>
 		</div>
 
-		<div class="col-lg-2 col-sm-3 col-md-3 col-xs-6 col-lan-xs-3 checkbox-container ">
-			<div class="checkbox-box square">
+	</div>
 
-			</div>
 
-		</div>
 
-		<div class="col-lg-2 col-sm-3 col-md-3 col-xs-6 col-lan-xs-3 checkbox-container">
-			<div class="checkbox-box square">
+</div>
 
-			</div>
 
-		</div>
-
-		<div class="col-lg-2 col-sm-3 col-md-3 col-xs-6 col-lan-xs-3 checkbox-container ">
-			<div class="checkbox-box square">
-
-			</div>
-
-		</div>
-
-	</div> -->
 
 </section>
