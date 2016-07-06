b43aac9883ca90ee8dcabd049420fbf615140331
Melania Miranda <melania.smc@hotmail.com>
2016-07-06 09:41:02 -0300

    9:40 6-07-2016

--- protected/views/static/recomendaciones.php
+++ protected/views/static/recomendaciones.php
@@ -10,7 +10,7 @@
 
 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 general-container-producir container-recomendaciones">
  	<!-- Barra de color -->
-	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 barra-recomendaciones">     </div>
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 hidden-xs barra-recomendaciones">     </div>
 
 	<!-- Primer Caja -->
 	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 box-recomendaciones">
@@ -22,17 +22,17 @@
 
 		<div class="col-lg-9 col-md-9 col-sm-9 col-xs-12 parrafo-recomendaciones parrafo-recomendaciones-1">
 			<p class="texto-blue-r">
-				Llega el otoño y nos encontramos, por un lado, con las vacas preñadas atravesando su último
-				tercio de la gestación y por el otro, con los terneros recién destetados que entran en la recría,
+				Llega el otoño y nos encontramos, por un lado, con las <b>vacas preñadas atravesando su último
+				tercio de la gestación</b> y por el otro, con <b>los terneros recién destetados que entran en la recría</b>,
 				etapa de transición hacia la invernada o hacia el entore en el caso de los  animales de reposición.
 				Generalmente esta categoría no es considerada prioritaria a nivel sanitario ni nutricional; sin embargo,
-				 es la etapa donde el animal en crecimiento es más eﬁciente para convertir alimento en músculo y hueso.
+				 <b>es la etapa donde el animal en crecimiento es más eﬁciente para convertir alimento en músculo y hueso</b>.
 			</p>
 			<p class="texto-blue-r">
 				Si queremos faenar animales de mayor peso en menor tiempo, llegar al entore a edad más temprana o
-				 que las vacas paran un ternero sano y fuerte, tenemos que implementar planes sanitarios preventivos
-				  y estratégicos acordes a la época, a la oferta forrajera y al destino del animal, para poder seguir
-					 en el camino a la Frontera Productiva del establecimiento.
+				 que las vacas paran un ternero sano y fuerte, tenemos que <b>implementar planes sanitarios preventivos
+				  y estratégicos</b> acordes a la época, a la oferta forrajera y al destino del animal, <b>para poder seguir
+					 en el camino a la Frontera Productiva</b> del establecimiento.
 			</p>
 		</div>
 
@@ -48,31 +48,31 @@
 		</div>
 		<div class="col-lg-9 col-md-9 col-sm-9 col-xs-12 parrafo-recomendaciones">
 			<p>
-				EL CONTROL PARASITOLÓGICO es muy importante en esta época (otoño-invierno), dado que el destete y la recría/invernada
+				<span>EL CONTROL PARASITOLÓGICO</span> es muy importante en esta época (otoño-invierno), dado que el destete y la recría/invernada
 				 son las categorías más sensibles a la acción de los parásitos.En Argentina, las pérdidas estimadas por ciclo productivo
 				  en una invernada con infestación parasitaria baja (subclínicas) son de 20 a 30 kg de peso vivo y pueden ascender
 					entre 40 y 60 kg ante infestaciones moderadas o altas (clínicas).El tratamiento con endectocidas de larga acción
-					(BOVIFORT®, BAGOMECTINA® FORTEAD3E, Bagomectina 3,15 LA AD3E y FLOK®) permite controlar por más tiempo las parasitosis
+					(<b>BOVIFORT®, BAGOMECTINA® FORTEAD3E, Bagomectina 3,15 LA AD3E y FLOK®</b>) permite controlar por más tiempo las parasitosis
 					 internas y externas (piojos, sarna, ura, garrapatas).Se recomienda la rotación de principios activos para evitar la
-					  generación de resistencia. Esta rotación puede hacerse con productos como PARAXANE® 10% (bencimidazol) o PARAXANE®
-						INYECTABLE (Ricobendazol) o FOSFAMISOL® (levamisol).
+					  generación de resistencia. Esta rotación puede hacerse con productos como <b>PARAXANE®</b> 10% (bencimidazol) o <b>PARAXANE®
+						INYECTABLE</b> (Ricobendazol) o <b>FOSFAMISOL®</b> (levamisol).
 			</p>
 
 			<p>
-				LA CARENCIA DE MINERALES Y/O VITAMINAS genera mermas en la ganancia diaria de peso que oscilan entre 3 y 17 kg
+				<span>LA CARENCIA DE MINERALES Y/O VITAMINAS</span> genera mermas en la ganancia diaria de peso que oscilan entre 3 y 17 kg
 				por animal.Las carencias pueden deberse a un bajo aporte diario en la dieta o a la interferencia de otros minerales
 				en la absorción (molibdeno, azufre, etc.).Hay regiones ganaderas muy importantes de nuestro país donde es bien conocida
 				 la carencia de cobre y zinc, como por ejemplo Cuenca del Salado. Además, hay que tener en cuenta que en situación de
 				  lluvias intensas y encharcamiento de pasturas las carencias minerales aumentan.Por lo expuesto, la estrategia de
-					 suplementación con cobre y zinc (SUPLENUT®) está indicada en situaciones de carencia de estos minerales.
+					 suplementación con cobre y zinc (<b>SUPLENUT®</b>) está indicada en situaciones de carencia de estos minerales.
 
 			</p>
 
 			<p>
-				INMUNIDAD CONTRA ENFERMEDADES INFECCIOSAS:  </br>
-    		<li>Síndrome respiratorio -> BIOPOLIGEN® HS / BIOPOLIGEN® AIR  </li>
-    		<li>Síndrome clostridial -> POLICLOSTRIGEN® / BIOCLOSTRIGEN® J5 / TRIPLEpoligen®   </li>
-    		<li>Síndrome de queratoconjuntivitis -> BIOQUERATOGEN® OLEO MAX   </li>
+				<span>INMUNIDAD CONTRA ENFERMEDADES INFECCIOSAS:</span>  </br>
+    		<li>Síndrome respiratorio -> <span>BIOPOLIGEN® HS / BIOPOLIGEN® AIR </span> </li>
+    		<li>Síndrome clostridial -> <span>POLICLOSTRIGEN® / BIOCLOSTRIGEN® J5 / TRIPLEpoligen®</span>   </li>
+    		<li>Síndrome de queratoconjuntivitis -> <span> BIOQUERATOGEN® OLEO MAX </span>  </li>
     		<li>Es preciso tener en cuenta que deben aplicarse dos dosis para cada síndrome, con un intervalo de 15 a 30 días,
 					cuando no se haya realizado vacunación al destete (primo vacunación).</li>
 			</p>
@@ -93,7 +93,7 @@
 		</div>
 		<div class="col-lg-9 col-md-9 col-sm-9 col-xs-12 parrafo-recomendaciones">
 			<p>
-				Para prevenir enfermedades en terneros recién nacidos se debe inmunizar (vacunar) a las madres en el último tercio de
+				Para <b>prevenir enfermedades en terneros recién nacidos se debe inmunizar (vacunar) a las madres</b> en el último tercio de
 				 gestación. Esto genera defensas en las vacas que son transmitidas al ternero a través del calostro.
 			</p>
 
@@ -108,18 +108,18 @@
 			</p>
 			<p>
 				Las vacunas a aplicar según el establecimiento, casuística y tipo de explotación son:  </br>
-    		<li>Síndrome de diarrea neonatal -> ROTATEC J5®   </li>
-    		<li>Síndrome respiratorio -> BIOPOLIGEN® HS / BIOPOLIGEN® AIR</li>
+    		<li>Síndrome de diarrea neonatal -> <span>ROTATEC J5®  </span> </li>
+    		<li>Síndrome respiratorio -> <span>BIOPOLIGEN® HS / BIOPOLIGEN® AIR</span></li>
 			</p>
 
 			<p>
-				En las zonas con carencias minerales de cobre y zinc, es vital la suplementación inyectable de SUPLENUT® aplicándolo a las
+				En las <b>zonas con carencias minerales de cobre y zinc</b>, es vital la suplementación inyectable de <b>SUPLENUT®</b> aplicándolo a las
 				vacas en el período de preparto para generar reserva hepática fetal y evitar la carencia en los primeros dos meses de vida de
 				 los terneros en los cuales la leche materna no cubre sus requerimientos.
 			</p>
 
 			<p>
-				Por último, en las vaquillonas en período de preparto, debe realizarse un estricto control parasitológico de manera que lleguen
+				Por último, en las vaquillonas en período de preparto, debe realizarse un <b>estricto control</b> parasitológico de manera que lleguen
 				 al parto con peso adecuado y buen estado corporal.
 			</p>
 
