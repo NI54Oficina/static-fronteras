14e0aed0b712aa37992f05cb515446475a454646
Melania Miranda <melania.smc@hotmail.com>
2016-07-06 10:26:45 -0300

    12:26 6-7-2016

--- protected/views/static/enfermedades.php
+++ protected/views/static/enfermedades.php
@@ -1,7 +1,7 @@
  <section id="" class="col-lg-12 col-md-12 col-sm-12 col-xs-12 section">
-<div class="col-xs-12 hidden-lg hidden-sm hidden-md hidden-xl titulo-mobile-producir titulo-mobile"> <p>Sanidad > Enfermedades</p> </div>
+<div class="col-xs-12  hidden-sm hidden-md  titulo-mobile-producir titulo-mobile"> <p>Sanidad > Enfermedades</p> </div>
 
-<div class="box-producir-3 col-lg-12 col-md-12 col-sm-12 col-xs-12 hidden-xs">
+<div class="box-producir-3 col-lg-12 col-md-12 col-sm-12 col-xs-12 hidden-xs hidden-lg">
 	<div class="box-sanidad border-shadow">
 		<div class="box-sanidad-inner ">
 			<p class="center-to-parent color-sanidad planes-h1">Enfermedades</p>
@@ -17,7 +17,7 @@
 		<!-- Enfermedades infecciosas  -->
 		<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/enfermedades-infecciosas"  class="">
 
-		<div class="col-lg-2-5 col-sm-4 col-md-2-5 col-xs-6 col-xl-5 col-lan-xs-4 max-box-planes">
+		<div class="col-lg-6 col-sm-4 col-md-2-5 col-xs-6 col-lan-xs-4 max-box-planes">
 			<div class=" col-lg-12 col-sm-12 col-md-12 col-xs-12 square planes box-sanidad">
 				<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12 planes-box  square enfermedades-box">
 
@@ -34,7 +34,7 @@
 
 		<!-- Parásitos externos -->
 		<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/parasitos-externos"  class="">
-			<div class="col-lg-2-5 col-sm-4 col-md-2-5 col-xs-6 col-xl-5 col-lan-xs-4  max-box-planes">
+			<div class="col-lg-6col-sm-4 col-md-2-5 col-xs-6  col-lan-xs-4  max-box-planes">
 			<div class=" col-lg-12 col-sm-12 col-md-12 col-xs-12 square planes box-sanidad">
 				<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12 planes-box  square enfermedades-box">
 
@@ -53,7 +53,7 @@
 
 		<!-- Parásitos internos -->
 		<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/parasitos-internos"  class="">
-			<div class="col-lg-2-5 col-sm-4 col-md-2-5 col-xs-6 col-xl-5 col-lan-xs-4  max-box-planes">
+			<div class="col-lg-6 col-sm-4 col-md-2-5 col-xs-6  col-lan-xs-4  max-box-planes">
 			<div class=" col-lg-12 col-sm-12 col-md-12 col-xs-12 square planes box-sanidad">
 				<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12 planes-box  square enfermedades-box">
 
@@ -72,7 +72,7 @@
 
 		<!--  -->
 		<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/aborto-bovino"  class="">
-			<div class="col-lg-2-5 col-sm-4 col-md-2-5 col-xs-6 col-xl-5  col-lan-xs-4 max-box-planes">
+			<div class="col-lg-6 col-sm-4 col-md-2-5 col-xs-6  col-lan-xs-4 max-box-planes">
 			<div class=" col-lg-12 col-sm-12 col-md-12 col-xs-12 square planes box-sanidad">
 				<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12 planes-box  square enfermedades-box">
 
